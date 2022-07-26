fe6c78b7b7318d6063c8cc4c53fedc3fc412a922
kendallli587 <kendallli587@gmail.com>
2022-07-26 12:34:00 -0600

    g

--- cell.py
+++ cell.py
@@ -11,4 +11,14 @@ class Cell:
             location,
             text='Text'
         )
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
+
+    def left_click_actions(self, event):
+        print(event)
+        print("left clicked")
+
+    def right_click_actions(self, event):
+        print(event)
+        print("right clicked")
