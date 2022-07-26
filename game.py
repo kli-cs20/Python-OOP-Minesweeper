8ea2a67c84800e61f68c039b0ec65a13ac6e70a7
kendallli587 <kendallli587@gmail.com>
2022-07-26 12:15:18 -0600

    w

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # Minesweeper with Object Oriented Programming
 from tkinter import *
 import settings
+import utils
 
 # Basic Window
 root = Tk()
@@ -12,16 +13,16 @@ root.resizable(False, False)
 top_frame = Frame(
     root,
     bg="red",
-    width=1440,
-    height=180
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg="blue",
-    width=360,
-    height=540
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
 )
 left_frame.place(x=0, y=180)
 
