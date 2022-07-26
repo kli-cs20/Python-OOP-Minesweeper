1b127f9de5390be948929111a51e13e1f384b7ed
kendallli587 <kendallli587@gmail.com>
2022-07-26 12:18:35 -0600

    Update game.py

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ root.resizable(False, False)
 
 top_frame = Frame(
     root,
-    bg="red",
+    bg="black",
     width=settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -20,11 +20,23 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg="blue",
+    bg="black",
     width=utils.width_prct(25),
     height=utils.height_prct(75)
 )
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25)
+)
+
 
 # Run the window
 root.mainloop()
